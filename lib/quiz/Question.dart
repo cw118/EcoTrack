e403e3918194886cba91ebc157d752d7c0792aca
elishamaq <45208241+elishamaq@users.noreply.github.com>
2022-01-08 18:46:46 -0500

    Changed odd number qs to stateful widgets

--- lib/quiz/Question.dart
+++ lib/quiz/Question.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import './Score.dart';
+
 //class for each question --> stores question & answer
 class Question {
   String text;
@@ -7,7 +10,14 @@ class Question {
   Question(this.text, this.score);
 }
 
-//class
+class Quiz {
+  int _questionNumber = 0;
+  int score = 0;
+
+  List<Question> _question = [
+    Question('What type of vehicle do you own?', 0),
+  ];
+}
 
 
 
